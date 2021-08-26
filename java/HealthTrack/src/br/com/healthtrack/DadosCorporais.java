2667f54e2495bc8176487393f54a4078724d20a4
Thiago de Sousa Rodrigues <thiago.np1@outlook.com>
2021-08-26 13:49:40 -0300

    duas classes com comentarios feito

--- java/HealthTrack/src/br/com/healthtrack/DadosCorporais.java
+++ java/HealthTrack/src/br/com/healthtrack/DadosCorporais.java
@@ -1,28 +1,47 @@
 package br.com.healthtrack;
 
+/**
+ * Classe que abstrai o cadastro de um peso.
+ * @author thiag_edf
+ * @version 1.0
+ */
 public class DadosCorporais {
+	
+	/**Peso do usuario.*/
 	private int peso;
+	
+	/**Data em que o cadastro foi realizado*/
 	private String data;
 	
 	public DadosCorporais() {
 		
 	}
 
+	/**
+	 * @return o valor da variavel peso.
+	 */
 	public int getPeso() {
 		return peso;
 	}
 
+	/**
+	 * @param passa o valor do parametro peso para a variavel peso.
+	 */
 	public void setPeso(int peso) {
 		this.peso = peso;
 	}
 
+	/**
+	 * @return o valor da variavel data.
+	 */
 	public String getData() {
 		return data;
 	}
 
+	/**
+	 * @param passa o valor do parametro data para a variavel data.
+	 */
 	public void setData(String data) {
 		this.data = data;
 	}
-	
-	
 }
--- java/HealthTrack/src/br/com/healthtrack/Usuario.java
+++ java/HealthTrack/src/br/com/healthtrack/Usuario.java
@@ -1,13 +1,34 @@
 package br.com.healthtrack;
-
+/**
+ * Classe que abstrai o cadastro de um usuario.
+ * @author thiag_edf
+ * @version 1.0
+ *
+ */
 public class Usuario {
-	public int cdUsuario;
+	
+	/*Codigo do usuario.**/
+	private int cdUsuario;
+	
+	/*Nome do usuario.**/
 	private String nome;
+	
+	/**Sobrenome do usuario.*/
 	private String sobrenome;
-	private String telCelular; 
+	
+	/**Numero do celular.*/
+	private String telCelular;
+	
+	/**Usuario*/
 	private String nmUser;
+	
+	/**Senha*/
 	private String nrsenha;
+	
+	/**Email*/
 	private String email;
+	
+	/**Numero do CPF*/
 	private int cpf;
 	
 	
@@ -32,74 +53,115 @@ public class Usuario {
 	}
 
 
+	/**
+	 * @return the nome
+	 */
 	public String getNome() {
 		return nome;
 	}
 
 
+	/**
+	 * @param nome the nome to set
+	 */
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
 
+	/**
+	 * @return the sobrenome
+	 */
 	public String getSobrenome() {
 		return sobrenome;
 	}
 
 
+	/**
+	 * @param sobrenome the sobrenome to set
+	 */
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
 	}
 
 
+	/**
+	 * @return the telCelular
+	 */
 	public String getTelCelular() {
 		return telCelular;
 	}
 
 
+	/**
+	 * @param telCelular the telCelular to set
+	 */
 	public void setTelCelular(String telCelular) {
 		this.telCelular = telCelular;
 	}
 
 
+	/**
+	 * @return the nmUser
+	 */
 	public String getNmUser() {
 		return nmUser;
 	}
 
 
+	/**
+	 * @param nmUser the nmUser to set
+	 */
 	public void setNmUser(String nmUser) {
 		this.nmUser = nmUser;
 	}
 
 
+	/**
+	 * @return the nrsenha
+	 */
 	public String getNrsenha() {
 		return nrsenha;
 	}
 
 
+	/**
+	 * @param nrsenha the nrsenha to set
+	 */
 	public void setNrsenha(String nrsenha) {
 		this.nrsenha = nrsenha;
 	}
 
 
+	/**
+	 * @return the email
+	 */
 	public String getEmail() {
 		return email;
 	}
 
 
+	/**
+	 * @param email the email to set
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
 
+	/**
+	 * @return the cpf
+	 */
 	public int getCpf() {
 		return cpf;
 	}
 
 
+	/**
+	 * @param cpf the cpf to set
+	 */
 	public void setCpf(int cpf) {
 		this.cpf = cpf;
 	}
-	
-	
+
 }
